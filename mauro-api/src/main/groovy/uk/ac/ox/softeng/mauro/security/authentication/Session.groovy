db349e10b6b9856bab3f997735f2508d27d11802
Regina Hart <regina.hart@answerdigital.com>
2024-10-17 15:36:47 +0100

    keycloak redirection with existing session based login - no authentication for idtoken

--- mauro-api/src/main/groovy/uk/ac/ox/softeng/mauro/security/authentication/Session.groovy
+++ mauro-api/src/main/groovy/uk/ac/ox/softeng/mauro/security/authentication/Session.groovy
@@ -1,11 +1,12 @@
-package uk.ac.ox.softeng.mauro.security.authentication.openidconnect
+package uk.ac.ox.softeng.mauro.security.authentication
 
 import jakarta.inject.Qualifier
-
 import java.lang.annotation.Retention
 import java.lang.annotation.RetentionPolicy
 
+
 @Qualifier
 @Retention(RetentionPolicy.RUNTIME)
-@interface IdToken {
+@interface Session {
 }
+
--- mauro-api/src/main/groovy/uk/ac/ox/softeng/mauro/security/authentication/openidconnect/MauroLoginHandler.groovy
+++ mauro-api/src/main/groovy/uk/ac/ox/softeng/mauro/security/authentication/openidconnect/MauroLoginHandler.groovy
@@ -2,14 +2,16 @@ package uk.ac.ox.softeng.mauro.security.authentication.openidconnect
 
 import groovy.transform.CompileStatic
 import io.micronaut.security.oauth2.endpoint.token.response.IdTokenLoginHandler
+import io.micronaut.security.session.SessionLoginHandler
 import jakarta.inject.Inject
 import jakarta.inject.Singleton
 import uk.ac.ox.softeng.mauro.security.authentication.MauroSessionLoginHandler
+import uk.ac.ox.softeng.mauro.security.authentication.Session
 
 @Singleton
 @CompileStatic
 class MauroLoginHandler  {
     @Inject @IdToken IdTokenLoginHandler idTokenLoginHandler
-    @Inject @IdToken.Session MauroSessionLoginHandler mauroSessionLoginHandler
+    @Inject @Session MauroSessionLoginHandler mauroSessionLoginHandler
 
 }
