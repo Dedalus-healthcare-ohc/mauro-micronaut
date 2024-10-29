bf2eb5111caf0a6723df7bef7bbea00eacea85bc
Regina Hart <regina.hart@answerdigital.com>
2024-10-29 11:33:41 +0000

    interim keycloakintegration test-relies on locally installed keycloak with client creds; test is ignored by default

--- mauro-api/src/test/groovy/uk/ac/ox/softeng/mauro/security/KeycloakIntegrationSpec.groovy
+++ mauro-api/src/test/groovy/uk/ac/ox/softeng/mauro/security/KeycloakIntegrationSpec.groovy
@@ -1,74 +1,97 @@
 package uk.ac.ox.softeng.mauro.security
 
-import dasniko.testcontainers.keycloak.KeycloakContainer
-import io.micronaut.core.annotation.Introspected
+import groovy.util.logging.Slf4j
 import io.micronaut.http.HttpRequest
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.HttpStatus
-import io.micronaut.http.MediaType
 import io.micronaut.runtime.EmbeddedApplication
 import jakarta.inject.Inject
 import jakarta.ws.rs.client.ClientBuilder
 import jakarta.ws.rs.client.Entity
 import jakarta.ws.rs.core.Form
+import jakarta.ws.rs.core.MediaType
 import jakarta.ws.rs.core.Response
 import org.jboss.resteasy.client.jaxrs.ResteasyClient
 import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder
 import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget
-import org.jboss.resteasy.spi.ResteasyAsynchronousContext
 import org.junit.Test
-import org.keycloak.admin.client.KeycloakBuilder
 import org.testcontainers.spock.Testcontainers
 import spock.lang.Shared
 import uk.ac.ox.softeng.mauro.persistence.SecuredContainerizedTest
 
-import java.net.http.HttpHeaders
-
 @Testcontainers
 @SecuredContainerizedTest
+@Slf4j
+@spock.lang.Ignore
 class KeycloakIntegrationSpec extends SecuredIntegrationSpec {
     @Shared
-    String authServerUrl
+    String authServerUrl = 'http://localhost:9009/realms/master/protocol/openid-connect/token'
+
 
     @Inject
     EmbeddedApplication<?> application
 
-    @Shared
+    //todo:fix this. Not working because unable to set realm-import file.
+    //Work-around: run keycloak locally on port 9009, get client-secret credentials, replace value below for token
+//    @Shared
+//    KeycloakContainer keycloak = new KeycloakContainer()
+//    //   .withRealmImportFile('sandbox-realm.json')
+//            .withAdminUsername("admin")
+//            .withAdminPassword("password")
 
-    KeycloakContainer keycloak  = new KeycloakContainer()
-            .withAdminUsername("admin")
-            .withAdminPassword("password")
 
-    void setupSpec(){
-        keycloak.withRealmImportFile("/sandbox.realm.json")
-        authServerUrl = keycloak.getAuthServerUrl() + '/realms/master/protocol/openid-connect/token'
+    void setupSpec() {
+        //  keycloak.start()
+        //  authServerUrl = keycloak.getAuthServerUrl() + '/realms/master/protocol/openid-connect/token'
+
     }
 
     void cleanupSpec() {
-        keycloak.close()
+//        keycloak.close()
     }
 
     @Test
-    void 'keycloak is running'() {
-        //   Map<String,String> env =  [ redirectUris: 'http://localhost:8088/oauth/callback/keycloak']
+    void 'can get access token from keycloak instance'() {
+//   not sure  how to use Keycloak instance
+//        Keycloak keycloakAdminClient = KeycloakBuilder.builder()
+//                .serverUrl(keycloak.getAuthServerUrl())
+//                .realm("master")
+//                .clientId("sandbox")
+//                .username(keycloak.getAdminUsername())
+//                .password(keycloak.getAdminPassword())
+//                .clientSecret('FR5Xz0TaymuAyPxGIqFRhf8HpBtwyOVi')
+//                .build();
 
+        //using Resteasy because micronaut httpclient does not support URLENCODED body
         Form form = new Form()
-        form.param('grant_type','password')
-        form.param('client_id','sandbox')
-        form.param('client_secret','FR5Xz0TaymuAyPxGIqFRhf8HpBtwyOVi')
-        form.param('username','admin')
-        form.param('password','password')
+        form.param('grant_type', 'password')
+        form.param('client_id', 'sandbox')
+        form.param('client_secret', 'FR5Xz0TaymuAyPxGIqFRhf8HpBtwyOVi')
+        form.param('username', 'admin')
+        form.param('password', 'password')
+
         Entity entity = Entity.form(form)
         ResteasyClient resteasyClient = ((ResteasyClientBuilder) ClientBuilder.newBuilder()).build()
         ResteasyWebTarget target = resteasyClient.target(authServerUrl)
+
         when:
-        Response resp = target.request(jakarta.ws.rs.core.MediaType.APPLICATION_FORM_URLENCODED_TYPE)
-        .post(entity)
+        Response resp = target.request(MediaType.APPLICATION_FORM_URLENCODED_TYPE)
+                .post(entity)
 
         then:
         resp
-//        resp.status == HttpStatus.OK
+        Map<String, String> responseEntity = resp.readEntity(Map.class)
+        resp.status == HttpStatus.OK.code
 
+        def currentAccessToken = responseEntity.get('access_token')
+
+                when:
+        HttpResponse response = client.toBlocking().exchange(HttpRequest.GET('/')
+                .header('Authorization', currentAccessToken), Map<String, String>.class)
+
+        then:
+        response
+        response.getStatus() == HttpStatus.OK
     }
 
 
